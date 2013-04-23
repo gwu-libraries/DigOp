05d8b41bf140e48c4a86ed3c31f665ae005b3ad1
Raza Gilani <gilani@library.gwu.edu>
2013-04-23 10:12:16 -0400

    remove3d GChartWrapper.charts from INSTALLED_APPS, closes #165

--- DigOp/settings.py
+++ DigOp/settings.py
@@ -134,8 +134,6 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
-    'bootstrapped',
-    'GChartWrapper.charts',
     'ui',
     'south',
     'profiles',
