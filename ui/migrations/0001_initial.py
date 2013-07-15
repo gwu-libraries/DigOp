869e5e61a5543d1cd3b2b3ef12e2c6e3fcc058ff
Raza Gilani <gilani@library.gwu.edu>
2013-07-15 15:05:49 -0400

    initial migration

--- ui/migrations/0001_initial.py
+++ ui/migrations/0001_initial.py
@@ -9,29 +9,32 @@ class Migration(SchemaMigration):
 
     def forwards(self, orm):
         # Adding model 'Project'
-        db.create_table('ui_project', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+        db.create_table(u'ui_project', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('name', self.gf('django.db.models.fields.CharField')(max_length=50)),
             ('description', self.gf('django.db.models.fields.TextField')()),
             ('startDate', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
             ('endDate', self.gf('django.db.models.fields.DateTimeField')(null=True, blank=True)),
             ('projectComplete', self.gf('django.db.models.fields.BooleanField')(default=False)),
         ))
-        db.send_create_signal('ui', ['Project'])
+        db.send_create_signal(u'ui', ['Project'])
+
+        # Adding unique constraint on 'Project', fields [u'id', 'name']
+        db.create_unique(u'ui_project', [u'id', 'name'])
 
         # Adding model 'Item'
-        db.create_table('ui_item', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+        db.create_table(u'ui_item', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('project', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['ui.Project'])),
             ('barcode', self.gf('django.db.models.fields.CharField')(max_length=50)),
             ('totalPages', self.gf('django.db.models.fields.IntegerField')(blank=True)),
             ('itemType', self.gf('django.db.models.fields.CharField')(max_length=10)),
         ))
-        db.send_create_signal('ui', ['Item'])
+        db.send_create_signal(u'ui', ['Item'])
 
         # Adding model 'ProcessingSession'
-        db.create_table('ui_processingsession', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+        db.create_table(u'ui_processingsession', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('item', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['ui.Item'])),
             ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
             ('identifier', self.gf('django.db.models.fields.CharField')(default='', max_length=30, blank=True)),
@@ -42,103 +45,106 @@ class Migration(SchemaMigration):
             ('startTime', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
             ('endTime', self.gf('django.db.models.fields.DateTimeField')()),
         ))
-        db.send_create_signal('ui', ['ProcessingSession'])
+        db.send_create_signal(u'ui', ['ProcessingSession'])
 
         # Adding model 'UserProfile'
-        db.create_table('ui_userprofile', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+        db.create_table(u'ui_userprofile', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('user', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['auth.User'], unique=True)),
             ('department', self.gf('django.db.models.fields.CharField')(max_length=10, blank=True)),
         ))
-        db.send_create_signal('ui', ['UserProfile'])
+        db.send_create_signal(u'ui', ['UserProfile'])
 
 
     def backwards(self, orm):
+        # Removing unique constraint on 'Project', fields [u'id', 'name']
+        db.delete_unique(u'ui_project', [u'id', 'name'])
+
         # Deleting model 'Project'
-        db.delete_table('ui_project')
+        db.delete_table(u'ui_project')
 
         # Deleting model 'Item'
-        db.delete_table('ui_item')
+        db.delete_table(u'ui_item')
 
         # Deleting model 'ProcessingSession'
-        db.delete_table('ui_processingsession')
+        db.delete_table(u'ui_processingsession')
 
         # Deleting model 'UserProfile'
-        db.delete_table('ui_userprofile')
+        db.delete_table(u'ui_userprofile')
 
 
     models = {
-        'auth.group': {
+        u'auth.group': {
             'Meta': {'object_name': 'Group'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
-            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
         },
-        'auth.permission': {
-            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
             'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
         },
-        'auth.user': {
+        u'auth.user': {
             'Meta': {'object_name': 'User'},
             'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
             'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
             'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
             'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
         },
-        'contenttypes.contenttype': {
+        u'contenttypes.contenttype': {
             'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
             'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
         },
-        'ui.item': {
+        u'ui.item': {
             'Meta': {'object_name': 'Item'},
             'barcode': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'itemType': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
-            'project': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['ui.Project']"}),
+            'project': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['ui.Project']"}),
             'totalPages': ('django.db.models.fields.IntegerField', [], {'blank': 'True'})
         },
-        'ui.processingsession': {
+        u'ui.processingsession': {
             'Meta': {'object_name': 'ProcessingSession'},
             'comments': ('django.db.models.fields.TextField', [], {'default': "''", 'blank': 'True'}),
             'endTime': ('django.db.models.fields.DateTimeField', [], {}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'identifier': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '30', 'blank': 'True'}),
-            'item': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['ui.Item']"}),
+            'item': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['ui.Item']"}),
             'operationComplete': ('django.db.models.fields.NullBooleanField', [], {'null': 'True', 'blank': 'True'}),
             'pagesDone': ('django.db.models.fields.IntegerField', [], {}),
             'startTime': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'task': ('django.db.models.fields.CharField', [], {'max_length': '4'}),
-            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']"})
         },
-        'ui.project': {
-            'Meta': {'object_name': 'Project'},
+        u'ui.project': {
+            'Meta': {'unique_together': "(('id', 'name'),)", 'object_name': 'Project'},
             'description': ('django.db.models.fields.TextField', [], {}),
             'endDate': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
             'projectComplete': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'startDate': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'})
         },
-        'ui.userprofile': {
+        u'ui.userprofile': {
             'Meta': {'object_name': 'UserProfile'},
             'department': ('django.db.models.fields.CharField', [], {'max_length': '10', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'user': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['auth.User']", 'unique': 'True'})
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'user': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['auth.User']", 'unique': 'True'})
         }
     }
 
